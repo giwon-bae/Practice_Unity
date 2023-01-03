2272d186626b3d5e0f54948471c883fa4471f259
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2023-01-03 23:22:45 +0900

    update 23-01-03
    study object pooling

--- Practice_Unity_Skill/Assets/Scripts/EnemySpawner.cs
+++ Practice_Unity_Skill/Assets/Scripts/EnemySpawner.cs
@@ -9,6 +9,18 @@ public class EnemySpawner : MonoBehaviour
 
     private int countEnemy = 0;
 
+    List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        pools = new List<GameObject>[enemyPrefabs.Length];
+
+        for(int i=0; i<pools.Length; i++)
+        {
+            pools[i] = new List<GameObject>();
+        }
+    }
+
     private void Update()
     {
         if(countEnemy <= 0)
@@ -32,5 +44,28 @@ public class EnemySpawner : MonoBehaviour
         GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
         Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+
+        foreach(GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if(select == null)
+        {
+            select = Instantiate(enemyPrefabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
     }
 }
