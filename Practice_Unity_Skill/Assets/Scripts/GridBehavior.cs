a943f7f852c3b9f28a6770ce3b109731339ba31a
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2023-01-11 03:49:32 +0900

    update 23-01-11
    study path finding

---
+++ Practice_Unity_Skill/Assets/Scripts/GridBehavior.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehavior : MonoBehaviour
+{
+    public int rows = 10;
+    public int columns = 10;
+    public int scale = 1;
+    public GameObject gridPrefab;
+    public Vector3 leftBottomLocation = new Vector3(0, 0, 0);
+    public GameObject[,] gridArray;
+    public int startX = 0;
+    public int startY = 0;
+    public int endX = 2;
+    public int endY = 2;
+
+
+    private void Awake()
+    {
+        gridArray = new GameObject[columns, rows];
+        if (gridPrefab)
+        {
+            GeneratedGrid();
+        }
+        else
+        {
+            print("missing gridprefab, please assign.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GeneratedGrid()
+    {
+        for(int i=0; i<columns; i++)
+        {
+            for(int j=0; j<rows; j++)
+            {
+                GameObject obj = Instantiate(gridPrefab, new Vector3(leftBottomLocation.x + scale * i, leftBottomLocation.y + scale * j, leftBottomLocation.z + scale * j), Quaternion.identity);
+                obj.transform.SetParent(gameObject.transform);
+                obj.GetComponent<GridStat>().x = i;
+                obj.GetComponent<GridStat>().y = j;
+                gridArray[i, j] = obj;
+            }
+        }
+    }
+
+    void InitialSetUp()
+    {
+        foreach(GameObject obj in gridArray)
+        {
+            obj.GetComponent<GridStat>().visited = -1;
+        }
+        gridArray[startX, startY].GetComponent<GridStat>().visited = 0;
+    }
+
+    bool TestDirection(int x, int y, int step, int direction)
+    {
+        // 1 - up, 2 - right, 3 - down, 4 - left
+        switch (direction)
+        {
+            case 1:
+                if (y + 1 < rows && gridArray[x, y + 1] && gridArray[x, y + 1].GetComponent<GridStat>().visited == step)
+                    return true;
+                else
+                    return false;
+            case 2:
+                if (x + 1 < columns && gridArray[x + 1, y] && gridArray[x + 1, y].GetComponent<GridStat>().visited == step)
+                    return true;
+                else
+                    return false;
+            case 3:
+                if (y - 1 > -1 && gridArray[x, y - 1] && gridArray[x, y - 1].GetComponent<GridStat>().visited == step)
+                    return true;
+                else
+                    return false;
+            case 4:
+                if (x - 1 > -1 && gridArray[x - 1, y] && gridArray[x - 1, y].GetComponent<GridStat>().visited == step)
+                    return true;
+                else
+                    return false;
+        }
+    }
+}
