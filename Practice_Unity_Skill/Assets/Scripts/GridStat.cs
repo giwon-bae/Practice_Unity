ec476e166e3430fe579ae1c62d7864df3555f729
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2023-01-21 18:36:36 +0900

    update 23-01-21
    made card script and card mangaer script

--- Practice_Unity_Skill/Assets/Scripts/GridStat.cs
+++ Practice_Unity_Skill/Assets/Scripts/GridStat.cs
@@ -11,11 +11,13 @@ public class GridStat : MonoBehaviour
     public int dirY;
 
     private Renderer rend;
+    private Color initialColor;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = gameObject.GetComponent<Renderer>();
+        initialColor = rend.material.color;
     }
 
     // Update is called once per frame
@@ -25,5 +27,19 @@ public class GridStat : MonoBehaviour
         {
             rend.material.color = Color.red;
         }
+        else
+        {
+            rend.material.color = initialColor;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        visited = -2;
+    }
+
+    private void OnMouseExit()
+    {
+        visited = -1;
     }
 }
